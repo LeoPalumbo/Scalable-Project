94d9a532e705f9c2de1bc5e04476f9531de5bc5c
davidedavoli <davide.davoli4@studio.unibo.it>
2022-03-15 21:08:16 +0100

    class controller e refactor (controller non testato)

--- src/main/scala/DistanceMatrix.scala
+++ src/main/scala/DistanceMatrix.scala
@@ -7,7 +7,7 @@ class FastaReader(val filename: String) extends Iterator[(String, String, String
   private lazy val reader = new BufferedReader(new FileReader(filename))
 
   class FastaReadException(string: String) extends Exception(string)
-  def hasNext() = reader.ready
+  def hasNext(): Boolean = reader.ready
   def next(): (String, String, String) = {
     // Read the tag line
     val tag = reader.readLine
@@ -21,7 +21,7 @@ class FastaReader(val filename: String) extends Iterator[(String, String, String
       if (line(0) != '>') sequencelist += line
       if (!reader.ready || line(0) == '>') {
         // Reached the end of the sequence
-        if (reader.ready) reader.reset
+        if (reader.ready) reader.reset()
         // Remove prepending '>'
         val tag2 = tag.drop(1).trim
         val id = tag2.split(Array(' ', '\t'))(0)
@@ -35,9 +35,9 @@ class FastaReader(val filename: String) extends Iterator[(String, String, String
 
 class NucleotideSequence(val filename: String){
   def read(position: Int): Option[Array[Char]]={
-    val parsed = new FastaReader(filename);
-    val sequences = parsed.map((x)=>x._3)
-    return sequences.drop(position).take(1).toList.headOption match{
+    val parsed = new FastaReader(filename)
+    val sequences = parsed.map(x=>x._3)
+     sequences.slice(position, position + 1).toList.headOption match{
       case None => None
       case Some(seq) => Some(seq.toArray)
     }
@@ -50,47 +50,47 @@ trait sequenceDistance[A]{
   def select3(a: Int, b: Int, c: Int): Int
   def penaltyBuilder(size:Int): Array[Int]
   def Compute [B >: A, C>: A](s1: Array[B], s2: Array[C]): (Int, Option[Double], Int, Int) ={
-    val firstLineDistances = penaltyBuilder(s2.size)
-    val firstColumnDistances = penaltyBuilder(s1.size)
+    val firstLineDistances = penaltyBuilder(s2.length)
+    val firstColumnDistances = penaltyBuilder(s1.length)
     val seqAndDist = s1.zip(firstColumnDistances)
 
-    def partialDist[B >: A](left: (Int, Int, Int), up:(Int, Int, Int), diag: (Int, Int, Int), c1: B, c2: B): (Int, Int, Int) = {
+    def partialDist[D >: A](left: (Int, Int, Int), up:(Int, Int, Int), diag: (Int, Int, Int), c1: D, c2: D): (Int, Int, Int) = {
       val selected = select3(left._1+penaltyLeft, up._1+penaltyUp, diag._1+Compare(c1, c2))	//si pensi min
       if (selected == diag._1+Compare(c1, c2) && c1 == c2){
-        val matched=diag._2+1;
-        val different=diag._3;
+        val matched=diag._2+1
+        val different=diag._3
         return (selected, matched, different)
       }
       if (selected == diag._1+Compare(c1, c2) && c1 != c2){
-        val matched=diag._2;
-        val different=diag._3+1;
+        val matched=diag._2
+        val different=diag._3+1
         return (selected, matched, different)
       }
       if (selected == up._1+penaltyUp){
-        val matched = up._2;
-        val different = up._3;
-        return (selected, matched, different)
+        val matched = up._2
+        val different = up._3
+        (selected, matched, different)
       }
       else //if (selected == left._1+penaltyLeft)
       {
-        val matched = left._2;
-        val different = left._3;
-        return (selected, matched, different)
+        val matched = left._2
+        val different = left._3
+        (selected, matched, different)
       }
     }
 
-    def makeNewLine[B >:A, C>:A] (oldLine: Array[(Int, Int, Int)], charLine: Array[C], leftDistance: Int, leftChar: B, diag: Int): Array[(Int, Int, Int)] = {
+    def makeNewLine[D >:A, E>:A](oldLine: Array[(Int, Int, Int)], charLine: Array[E], leftDistance: Int, leftChar: D, diag: Int): Array[(Int, Int, Int)] = {
       val charsAndDists = oldLine.zip(charLine).scanLeft(((leftDistance, 0, 0), (diag, 0, 0)))( (acc, item) => (partialDist(acc._1, item._1, acc._2, leftChar, item._2), item._1)) //il secondo elemento è la diagonale, è ottenuto ad ogni passo mettendo item._1, quindi l'elemento che attualmente si trova esattamente sopra
-      charsAndDists.drop(1).map((x)=> x._1)
+      charsAndDists.drop(1).map(x=> x._1)
     }
 
 
-    val result= seqAndDist.foldLeft((firstLineDistances, Array.fill(firstLineDistances.size)(0), Array.fill(firstLineDistances.size)(0)).zipped.toArray)((acc, item)=> (makeNewLine(acc, s2, item._2, item._1, if(acc(0)._1==0) 0 else item._2))) //l'if serve per il primissimo valore, quello in M[0,0]
+    val result= seqAndDist.foldLeft((firstLineDistances, Array.fill(firstLineDistances.length)(0), Array.fill(firstLineDistances.length)(0)).zipped.toArray)((acc, item)=> makeNewLine(acc, s2, item._2, item._1, if(acc(0)._1==0) 0 else item._2)) //l'if serve per il primissimo valore, quello in M[0,0]
 
-    val p= if (result(s2.length-1)._2+result(s2.length-1)._3!=0) Some((result(s2.length-1)._3).toDouble/(result(s2.length-1)._2+result(s2.length-1)._3).toDouble) else None
-    (result(s2.length-1)._1, p, (result(s2.length-1)._3), (result(s2.length-1)._2)); //1: score, 2: p-distance, 3: sostituzioni, 4: corretti
+    val p= if (result(s2.length-1)._2+result(s2.length-1)._3!=0) Some(result(s2.length-1)._3.toDouble/(result(s2.length-1)._2+result(s2.length-1)._3).toDouble) else None
+    (result(s2.length-1)._1, p, result(s2.length-1)._3, result(s2.length-1)._2); //1: score, 2: p-distance, 3: sostituzioni, 4: corretti
   }
-  def Compare [B>: A, C>: A](m1: B, m2: C): Int
+  def Compare [B>: A](m1: B, m2: B): Int
 
   def Score[B >: A, C>: A](s1: Array[B], s2: Array[C]): Int ={Compute(s1, s2)._1}
   def pDistance[B >: A, C>: A](s1: Array[B], s2: Array[C]): Option[Double]={Compute(s1, s2)._2}
@@ -99,19 +99,19 @@ trait sequenceDistance[A]{
 }
 
 class lDistance extends sequenceDistance[Char]{
-  def penaltyLeft = 1;
-  def penaltyUp = 1;
+  def penaltyLeft = 1
+  def penaltyUp = 1
   def penaltyBuilder(size:Int): Array[Int] = (0 until size).toArray
-  def select3(a: Int, b: Int, c: Int) = Math.min(Math.min(a, b), c);
-  def Compare [B>: Char, C>: Char](c1: B, c2: C): Int = {if (c1==c2) 0 else 1}
+  def select3(a: Int, b: Int, c: Int): Int = Math.min(Math.min(a, b), c)
+  def Compare [B>: Char](c1: B, c2: B): Int = {if (c1==c2) 0 else 1}
 }
 
 class NeighbourJoining {
 
-  var r_i = Map[Int, Double]()
-  var distances = Map[(Int, Int), Double]()
-  var d_i_j = Map[(Int, Int), Double]()
-  var graph = Map[(Int, Int), Double]()
+  var r_i: Map[Int, Double] = Map[Int, Double]()
+  var distances: Map[(Int, Int), Double] = Map[(Int, Int), Double]()
+  var d_i_j: Map[(Int, Int), Double] = Map[(Int, Int), Double]()
+  var graph: Map[(Int, Int), Double] = Map[(Int, Int), Double]()
   var numOfSeq: Int = 0
   var numOfNodeTmp : Int = 0
   var last_node: Int = 0
@@ -148,7 +148,7 @@ class NeighbourJoining {
 
 
   def setR_I(): Unit ={
-    r_i=distances.groupBy(x=> x._1._1 ).mapValues((x)=>x.foldLeft(0.0)((e, x)=> e+x._2)).mapValues(x=> x/(numOfSeq - 2))
+    r_i=distances.groupBy(x=> x._1._1 ).mapValues(x=>x.foldLeft(0.0)((e, x)=> e+x._2)).mapValues(x=> x/(numOfSeq - 2))
   }
 
 
@@ -172,24 +172,24 @@ class NeighbourJoining {
 
   def updateMatrix(node_1: Int, node_2: Int): Unit ={
     val x = distances.filterKeys(k => k._1 != node_1 && k._1 != node_2)
-      .groupBy((x)=>x._1._1).flatMap(
+      .groupBy(x=>x._1._1).flatMap(
       x =>  {
-        val d = (x._2.filter(_._1._2==node_1).head._2 + x._2.filter(_._1._2==node_2).head._2- distances(node_1, node_2))/2;
+        val d = (x._2.filter(_._1._2==node_1).head._2 + x._2.filter(_._1._2==node_2).head._2- distances(node_1, node_2))/2
         Seq(((x._1, last_node), d),((last_node, x._1), d))
       })
 
     distances = (distances.filterKeys(k => k._1 != node_1 && k._1 != node_2 && k._2 != node_1 && k._2 != node_2) ++ x) +((last_node, last_node)->0.0)
-    numOfSeq-=1;
+    numOfSeq-=1
     last_node = last_node + 1
   }
 }
 
 class ParNeighbourJoining(sc: SparkContext) {
 
-  var r_i = Map[Int, Double]()
-  var distances : RDD[((Int, Int), Double)]= sc.emptyRDD[((Int, Int), Double)];
-  var d_i_j : RDD[((Int, Int), Double)]= sc.emptyRDD[((Int, Int), Double)];
-  var graph : RDD[((Int, Int), Double)]= sc.emptyRDD[((Int, Int), Double)];
+  var r_i: Map[Int, Double] = Map[Int, Double]()
+  var distances : RDD[((Int, Int), Double)]= sc.emptyRDD[((Int, Int), Double)]
+  var d_i_j : RDD[((Int, Int), Double)]= sc.emptyRDD[((Int, Int), Double)]
+  var graph : RDD[((Int, Int), Double)]= sc.emptyRDD[((Int, Int), Double)]
   //var numOfSeq: Int = 0
   var numOfNodeTmp : Int = 0
   var last_node: Int = 0
@@ -200,18 +200,17 @@ class ParNeighbourJoining(sc: SparkContext) {
     //var distances : RDD[((Int, Int), Double)]= sc.emptyRDD[((Int, Int), Double)];
     //var d_i_j : RDD[((Int, Int), Double)]= sc.emptyRDD[((Int, Int), Double)];
 
-    distances ++= (sc.parallelize(d.toSeq).flatMap[((Int, Int), Double)](((x: ((Int, Int), Double))=> Seq[((Int, Int), Double)](((x._1._1, x._1._2), x._2), ((x._1._2, x._1._1), x._2)))))
-    var numOfSeq = distances.max._1._1 + 1
+    distances ++= sc.parallelize(d.toSeq).flatMap[((Int, Int), Double)]((x: ((Int, Int), Double))=> Seq[((Int, Int), Double)](((x._1._1, x._1._2), x._2), ((x._1._2, x._1._1), x._2)))
+    val numOfSeq = distances.max._1._1 + 1
     numOfNodeTmp = distances.max._1._1 + 1
     last_node = numOfSeq
-    distances.union(sc.parallelize((0 until numOfSeq)).map((x)=> ((x, x) -> 0.0)))
+    distances.union(sc.parallelize(0 until numOfSeq).map(x=> (x, x) -> 0.0))
 
 
-    var n : (Int, Int) = (0, 0)
     (0 until (numOfSeq - 2)).foreach (i =>{
 
-      r_i=distances.groupBy(x=> x._1._1 ).mapValues((x)=>x.foldLeft(0.0)((e, x)=> e+x._2)).mapValues(x=> x/(numOfSeq-i - 2)).collect().toMap
-      d_i_j = distances.filter(x=> x._1._1>x._1._2).map((x) => (x._1 ,x._2 - r_i(x._1._1) - r_i(x._1._2)))
+      r_i=distances.groupBy(x=> x._1._1 ).mapValues(x=>x.foldLeft(0.0)((e, x)=> e+x._2)).mapValues(x=> x/(numOfSeq-i - 2)).collect().toMap
+      d_i_j = distances.filter(x=> x._1._1>x._1._2).map(x => (x._1 ,x._2 - r_i(x._1._1) - r_i(x._1._2)))
 
 
       val min = d_i_j.min()(Ordering[Double].on[((Int, Int),Double)](_._2))
@@ -224,41 +223,98 @@ class ParNeighbourJoining(sc: SparkContext) {
       graph ++= sc.parallelize(Seq(((node_1, numOfSeq+i) , dist_node1)))
       graph ++= sc.parallelize(Seq(((node_2, numOfSeq+i) , dist_node2)))
 
-      val dist = distances.lookup(node_1, node_2).head;
+      val dist = distances.lookup(node_1, node_2).head
 
       val x = distances.filter(x => x._1._1 != node_1 && x._1._1 != node_2)
-        .groupBy((x)=>x._1._1).flatMap(
+        .groupBy((x: ((Int, Int), Double)) =>x._1._1).flatMap(
         x =>  {
-          val d = (x._2.filter(_._1._2==node_1).head._2 + x._2.filter(_._1._2==node_2).head._2- dist)/2;
+          val d = (x._2.filter(_._1._2==node_1).head._2 + x._2.filter(_._1._2==node_2).head._2- dist)/2
           Seq(((x._1, numOfSeq+i), d),((numOfSeq+i, x._1), d))
         })
       distances = (distances.filter(x => x._1._1 != node_1 && x._1._1 != node_2 && x._1._2 != node_1 && x._1._2 != node_2) ++ x) ++ sc.parallelize(Seq((last_node, last_node)->0.0))
     })
     graph ++=sc.parallelize(Seq((distances.filter(x=> x._1._1<x._1._2).first()._1._1, distances.filter(x=> x._1._1<x._1._2).first()._1._2) -> distances.filter(x=> x._1._1<x._1._2).first()._2))
   }
+}
 
+class Controller(par_matrix: Boolean,
+                 par_joining: Boolean,
+                 metric: String,
+                 filedirs: List[String],
+                 max_seq_per_file: Integer,
+                 sc: SparkContext
+                ) {
 
-  def init(): Unit = {
-
+  def computeD(f : (Array[Char], Array[Char]) => Double)(x: ((Int, Array[Char]), (Int, Array[Char]))) : ((Int, Int), Double) = {
+    ((x._1._1, x._2._1), f(x._1._2, x._2._2))
   }
+  def computePDist(tuple: ((Int, Array[Char]), (Int, Array[Char]))): ((Int, Int), Double) = computeD(f = (s1, s2) => {
+    val ld = new lDistance
+    ld.pDistance(s1, s2) match {
+      case None => 1.0
+      case Some(d) => d
+    }
+  })(tuple)
 
+  def computeSubsDist(tuple: ((Int, Array[Char]), (Int, Array[Char]))): ((Int, Int), Double) = computeD((s1, s2)=>{
+    val ld = new lDistance; ld.substitutions(s1, s2)})(tuple)
 
-  def setR_I(): Unit ={
-  }
+  def run (): Map[(Int, Int), Double] ={
+    val files: List[String] = filedirs.flatMap(z => new java.io.File(z).listFiles.filter(_.getName.endsWith(".fasta")).map(x=>z+"/"+x.getName))
+    val data: List[((Array[Char], Array[Char], Array[Char]), Int)] = files.flatMap(x=> new FastaReader(x).toList.take(max_seq_per_file).map(z=> (z._1.toArray, z._2.toArray, z._3.toArray))) zip files.indices
+    //data._1 = id
+    //data._2 = tag \in id
+    //data._3 = sequence
 
+    //val sequences = files.map((x)=> new NucleotideSequence(x).read(0))
 
-  def setD_I_J(): Unit ={
-  }
+    var pairs : List[((Int, Array[Char]), (Int, Array[Char]))] = List[((Int, Array[Char]), (Int, Array[Char]))]()
 
-  def joinSmallestNodes() ={
-  }
+    for (i <- data.indices){
+      for (j<- 0 until i){
+        pairs = pairs :+ ((i, data(i)._1._3), (j, data(j)._1._3))
+      }
+    }
 
-  def updateMatrix(node_1: Int, node_2: Int): Unit ={
+    var distances = Map[(Int, Int), Double]()
+
+    if (par_matrix) {
+      val ppairs = sc.parallelize(pairs)
+      if (metric == "substitutions") {
+        distances = ppairs.map((x: ((Int, Array[Char]), (Int, Array[Char]))) => computeSubsDist(x)).collect().toMap
+      }
+      if (metric == "p") {
+        distances = ppairs.map(x => computePDist(x)).collect().toMap
+      }
+    }
+    else {
+      if (metric == "substitutions") {
+        distances = pairs.map(x => computeSubsDist(x)).toMap
+      }
+      if (metric == "p") {
+        distances = pairs.map(x => computePDist(x)).toMap
+      }
+    }
+
+    if (par_joining) {
+      val neighbourJoining = new NeighbourJoining()
+      neighbourJoining.init(distances)
+      neighbourJoining.NJ()
+      neighbourJoining.graph
+    }
+
+    else {
+      val parNeighbourJoining = new ParNeighbourJoining(sc)
+      parNeighbourJoining.NJ(distances)
+      parNeighbourJoining.graph.collect().toMap
+    }
+
   }
 }
 
+
 object main{
-  def main(argv: Array[String]){
+  def main(argv: Array[String]): Unit = {
     val conf = new SparkConf().setAppName("Phylogenetic Tree").setMaster("local[*]")
     val sc = new SparkContext(conf)
 
@@ -315,7 +371,7 @@ object main{
 
     */
     //println(distances)
-    var dist : Map[(Int, Int), Double] = Map((3,2) -> 14, (3,1) -> 18, (3,0) -> 27, (2,1) -> 12, (2,0) -> 21, (1,0) -> 17)
+    val dist: Map[(Int, Int), Double] = Map((3, 2) -> 14, (3, 1) -> 18, (3, 0) -> 27, (2, 1) -> 12, (2, 0) -> 21, (1, 0) -> 17)
 
     val neighbourJoining = new NeighbourJoining()
     neighbourJoining.init(dist)
