721956282a993d9420fbd962a4a19bd1e513c811
LeoPalumbo <palumboleonardopio@gmail.com>
2022-04-05 14:19:58 +0200

    add num worker in script

--- src/gcpScript/04.dataproc-run-job.sh
+++ src/gcpScript/04.dataproc-run-job.sh
@@ -7,6 +7,7 @@
       --class ${SCALA_RUNNABLE_CLASS} \
       --jars ${SCALA_JAR_FILE_FOR_JOB_SUBMIT} \
       -- $JAR_FILE $PAR_METRICS $PAR_JOINING $METRIC $MAX_SEQUENCES_PER_FILE $ALPHA $BETA $GAMMA
+      #--num-workers 5
       #--bucket gs://scala-project-data-bucket/
       #gs://${GCS_BUCKET_NAME}/${DATA_FILE} gs://${GCS_BUCKET_NAME}/output/${RUN_SESSION}/
 ./90.log-time.sh "SPARK JOB '${SCALA_RUNNABLE_CLASS}' DONE!"
